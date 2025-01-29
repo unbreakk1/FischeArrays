99fc49babb563ccd5d14aaeeb483548b011b2e5b
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-01-29 11:42:49 +0100

    add Array Bonus

---
+++ src/main/java/arrays/EasyArrayBonus.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class EasyArrayBonus
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input prompt
+        System.out.print("Enter Text: ");
+        String input = scanner.nextLine();
+
+        // letter output
+        System.out.println("Letters in Input: ");
+        for (char c : input.toCharArray())
+        {
+            if (Character.isLetter(c))
+                System.out.println(c);
+        }
+        System.out.println();
+    }
+
+}
